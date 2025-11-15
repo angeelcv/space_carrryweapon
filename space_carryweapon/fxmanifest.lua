3b8bc63b32c38b6268d8ed0ce350d4d2beac11a0
angeelcv03 <angeelcv03@gmail.com>
2025-11-15 01:08:23 +0100

    Update fxmanifest.lua

--- space_carryweapon/fxmanifest.lua
+++ space_carryweapon/fxmanifest.lua
@@ -1,11 +1,12 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'angeelcv03 & Space'
-discord 'https://discord.gg/MDFSqr6fJz'
+author 'angeelcv03'
+discord 'https://discord.gg/fmnp3gXWar'
 description 'Cambio de animación al sostener un arma'
 version '1.0.0'
 
 client_scripts {
     'client.lua'
 }
+
